8d5ced6bb8b71a3139e148392367d56040d510c1
kofasusi <kacperpocztaa@gmail.com>
2025-04-27 09:30:59 +0200

    flags

--- cz1.sh
+++ cz1.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 case "$1" in
---date)
+--date|-d)
 date
 ;;
---logs)
+--logs|-l)
 for i in {1..100}
 do
 echo "stworzono log$i.txt o $(date)" > "log$i.txt"
 done
 ;;
---logs30)
+--logs30|-l30)
 for i in {1..30}
 do
 echo "stworzono log$i.txt o $(date)" > "log$i.txt"
 done
 ;;
---help)
-echo "--date : dzisiejsza data"
-echo "--logs : tworzy 100 plików log"
-echo "--logs30 : tworzy 30 plików log"
+--help| -h)
+echo "--date lub -d: dzisiejsza data"
+echo "--logs lub -l : tworzy 100 plikow log"
+echo "--logs30 lub -l30: tworzy 30 plikow log"
 ;;
+
 *)
 echo "Nieznana opcja"
 ;;
