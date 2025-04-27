67f4a4ddc52f4a4682c75cd3490cd384e1e01388
kofasusi <kacperpocztaa@gmail.com>
2025-04-27 09:34:57 +0200

    error i gitignore

--- cz1.sh
+++ cz1.sh
@@ -20,11 +20,19 @@ echo "--date lub -d: dzisiejsza data"
 echo "--logs lub -l : tworzy 100 plikow log"
 echo "--logs30 lub -l30: tworzy 30 plikow log"
 echo "--init : klonuje repo i ustawia PATH"
+echo "--error lub -e : tworzy pliki errorx.txt"
 ;;
 --init)
 git clone https://github.com/kofasusi?bashing.git
 export PATH=$PATH:$(pwd)
 echo "Sciezka $(pwd) dodana do PATH"
+;;
+--error|-e)
+error_number=${2:-100}
+for i in $(seq 1 $error_number)
+do
+    echo "error error$i.txt stworzony $(date)" > "error$i.txt"
+done
 ;;
 *)
 echo "Nieznana opcja"
